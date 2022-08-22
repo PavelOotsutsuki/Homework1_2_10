9d7f7d1df4986d15721d9d2e240b1f5059abf735
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-08-22 16:06:08 +0300

    Добавьте файлы проекта.

---
+++ Homework1_2_10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Homework1_2_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int enterNumber = random.Next(0, 100000);
+            int powNumber = 2;
+            int indexNumber = 2;
+            int maxNumber;
+            int powCount = 0;
+
+            for(maxNumber=indexNumber; maxNumber<enterNumber; maxNumber*=powNumber)
+            {
+                powCount++;
+            }
+
+            Console.WriteLine("Для числа " + enterNumber + " максимальное число равно " + maxNumber + ". Это 2 в " + powCount + " степени.");         
+        }
+    }
+}
